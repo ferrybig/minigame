ccc536b466ca0bfce44c5fa427d1e884eae8e59b
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2016-02-25 23:01:10 +0100

    Remove joinable system for stable build

--- src/main/java/me/ferrybig/javacoding/minecraft/minigame/Controller.java
+++ src/main/java/me/ferrybig/javacoding/minecraft/minigame/Controller.java
@@ -2,7 +2,6 @@ package me.ferrybig.javacoding.minecraft.minigame;
 
 import java.util.Collection;
 import java.util.List;
-import me.ferrybig.javacoding.minecraft.party.util.Joinable;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 
@@ -19,29 +18,5 @@ public interface Controller {
 	public int getSlotsLeft();
 	
 	public Collection<? extends OfflinePlayer> getPendingPlayers();
-	
-	public default Joinable<OfflinePlayer, Player> asJoinable() {
-		return new Joinable<OfflinePlayer, Player>() {
-			@Override
-			public long freeSlotsLeft() {
-				return getSlotsLeft();
-			}
-
-			@Override
-			public boolean joinOfflinePlayer(List<? extends OfflinePlayer> players) {
-				return tryAddPlayer(players);
-			}
-
-			@Override
-			public void joinOnlinePlayer(Player player) {
-				addPlayer(player);
-			}
-
-			@Override
-			public void quitOfflinePlayer(List<? extends OfflinePlayer> players) {
-				players.forEach(Controller.this::removePlayer);
-			}
-		};
-	};
 			
 }
