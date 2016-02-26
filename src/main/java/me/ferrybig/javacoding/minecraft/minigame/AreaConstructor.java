db26e0c2ac31ca306ddff15b9dbcd7bf3592dfbc
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2016-02-26 16:48:39 +0100

    Define area constructors

---
+++ src/main/java/me/ferrybig/javacoding/minecraft/minigame/AreaConstructor.java
@@ -0,0 +1,7 @@
+
+package me.ferrybig.javacoding.minecraft.minigame;
+
+public interface AreaConstructor {
+
+	public Area construct(AreaInformation info);
+}
---
+++ src/main/java/me/ferrybig/javacoding/minecraft/minigame/AreaContextConstructor.java
@@ -0,0 +1,7 @@
+
+package me.ferrybig.javacoding.minecraft.minigame;
+
+public interface AreaContextConstructor {
+
+	public AreaContext construct(AreaInformation area);
+}
