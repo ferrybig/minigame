926793e2fdc79b1554d5be0015377f89ac21fdc1
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2016-02-29 12:54:55 +0100

    Add example translations

--- src/main/java/me/ferrybig/javacoding/minecraft/minigame/translation/BaseTranslation.java
+++ src/main/java/me/ferrybig/javacoding/minecraft/minigame/translation/BaseTranslation.java
@@ -2,6 +2,8 @@ package me.ferrybig.javacoding.minecraft.minigame.translation;
 
 public enum BaseTranslation implements Translation {
 
+	AREA_PLAYER_JOINED("area.player.join"),
+	AREA_PLAYER_LEFT("area.player.left"),
 	;
 
 	private final String key;
