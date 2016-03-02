0e6ea4c40d78188d440658babb7df33e1c56efd9
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2016-03-02 13:26:39 +0100

    DefaultAreInformation

---
+++ src/main/java/me/ferrybig/javacoding/minecraft/minigame/AreaInformationBuilder.java
@@ -0,0 +1,66 @@
+
+package me.ferrybig.javacoding.minecraft.minigame;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import org.bukkit.Location;
+import org.bukkit.block.Block;
+
+public class AreaInformationBuilder {
+
+	private String name;
+	private boolean enabled = true;
+	private String description = "";
+	private Map<String, List<Block>> taggedBlocks = Collections.emptyMap();
+	private Map<String, List<Location>> taggedLocations = Collections.emptyMap();
+	private Selection bounds;
+	private int maxPlayers = Byte.MAX_VALUE;
+	
+	public AreaInformationBuilder() {
+	}
+
+	public AreaInformationBuilder(String name) {
+		this.name = name;
+	}
+
+	public AreaInformationBuilder setName(String name) {
+		this.name = name;
+		return this;
+	}
+
+	public AreaInformationBuilder setEnabled(boolean enabled) {
+		this.enabled = enabled;
+		return this;
+	}
+
+	public AreaInformationBuilder setDescription(String description) {
+		this.description = description;
+		return this;
+	}
+
+	public AreaInformationBuilder setTaggedBlocks(Map<String, java.util.List<org.bukkit.block.Block>> taggedBlocks) {
+		this.taggedBlocks = taggedBlocks;
+		return this;
+	}
+
+	public AreaInformationBuilder setTaggedLocations(Map<String, java.util.List<org.bukkit.Location>> taggedLocations) {
+		this.taggedLocations = taggedLocations;
+		return this;
+	}
+
+	public AreaInformationBuilder setBounds(Selection bounds) {
+		this.bounds = bounds;
+		return this;
+	}
+
+	public AreaInformationBuilder setMaxPlayers(int maxPlayers) {
+		this.maxPlayers = maxPlayers;
+		return this;
+	}
+
+	public AreaInformation createAreaInformation() {
+		return new DefaultAreaInformation(name, enabled, description, taggedBlocks, taggedLocations, bounds, maxPlayers);
+	}
+
+}
---
+++ src/main/java/me/ferrybig/javacoding/minecraft/minigame/DefaultAreaInformation.java
@@ -0,0 +1,71 @@
+
+package me.ferrybig.javacoding.minecraft.minigame;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import org.bukkit.Location;
+import org.bukkit.block.Block;
+
+public class DefaultAreaInformation implements AreaInformation {
+	
+	private final String description;
+	private final String name;
+	private final boolean enabled;
+	private final Map<String, List<Block>> taggedBlocks;
+	private final Map<String, List<Location>> taggedLocations;
+	private final Selection bounds;
+	private final int maxPlayers;
+	
+	public DefaultAreaInformation(String name, boolean enabled, String description, 
+			Map<String, List<Block>> taggedBlocks, 
+			Map<String, List<Location>> taggedLocations, Selection bounds, int maxPlayers) {
+		this.description = Objects.requireNonNull(description);
+		this.name = Objects.requireNonNull(name);
+		this.taggedBlocks = Objects.requireNonNull(taggedBlocks);
+		this.taggedLocations = Objects.requireNonNull(taggedLocations);
+		this.bounds = Objects.requireNonNull(bounds);
+		this.maxPlayers = Objects.requireNonNull(maxPlayers);
+		this.enabled = enabled;
+	}
+
+	@Override
+	public Selection getBounds() {
+		return bounds;
+	}
+
+	@Override
+	public String getDescription() {
+		return description;
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public Map<String, List<Block>> getTaggedBlocks() {
+		return taggedBlocks;
+	}
+
+	@Override
+	public Map<String, List<Location>> getTaggedLocations() {
+		return taggedLocations;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	@Override
+	public int maxPlayers() {
+		return maxPlayers;
+	}
+	
+	public static AreaInformationBuilder builder(String name) {
+		return new AreaInformationBuilder(name);
+	}
+
+}
