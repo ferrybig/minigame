8bc8b0dc6d77d06130e6a7429b14ac0e28cc9272
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2016-03-03 10:09:06 +0100

    Expanded EventExecutor with run loop

--- src/main/java/me/ferrybig/javacoding/minecraft/minigame/executors/BukkitEventExecutor.java
+++ src/main/java/me/ferrybig/javacoding/minecraft/minigame/executors/BukkitEventExecutor.java
@@ -3,8 +3,10 @@ package me.ferrybig.javacoding.minecraft.minigame.executors;
 import io.netty.util.concurrent.AbstractScheduledEventExecutor;
 import io.netty.util.concurrent.Promise;
 import java.util.Collection;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Objects;
+import java.util.Queue;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
@@ -12,46 +14,77 @@ import java.util.concurrent.FutureTask;
 import java.util.concurrent.RunnableFuture;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
+import java.util.logging.Level;
 import java.util.stream.Collectors;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.scheduler.BukkitTask;
 
 public class BukkitEventExecutor extends AbstractScheduledEventExecutor {
 
+	private static final int MAX_EXECUTION_DEPTH = 4;
 	private final Plugin plugin;
 	private int executionDepth = 0;
 	private boolean shuttingDown;
 	private final Promise<?> terminationFuture;
-	private final BukkitTask task;
+	private final BukkitTask scheduledTask;
+	private final Queue<Runnable> scheduledTasks = new LinkedList<>();
 
 	public BukkitEventExecutor(Plugin plugin) {
 		this.plugin = Objects.requireNonNull(plugin);
 		this.terminationFuture = super.newPromise();
-		this.task = this.plugin.getServer().getScheduler().runTaskTimer(plugin, () -> {
+		this.scheduledTask = this.plugin.getServer().getScheduler().runTaskTimer(plugin, () -> {
 			Runnable timed;
-			while((timed = pollScheduledTask())!= null)
+			runLoop();
+			while ((timed = pollScheduledTask()) != null) {
 				timed.run();
+			}
 		}, 1, 1);
 	}
-	
+
 	@Override
 	public boolean awaitTermination(long timeout, TimeUnit unit) throws InterruptedException {
 		return terminationFuture.await(timeout, unit);
 	}
 
-	@Override
-	public void execute(Runnable command) {
-		if(inEventLoop() && executionDepth < 4) {
+	private void runLoop() {
+		assert inEventLoop();
+		Runnable task;
+		while ((task = scheduledTasks.poll()) != null) {
+			task.run();
+		}
+	}
+	
+	private void tryRunLoop() {
+		assert inEventLoop();
+		if (executionDepth < MAX_EXECUTION_DEPTH) {
 			executionDepth++;
 			try {
-				command.run();
+				runLoop();
 			} finally {
 				assert executionDepth > 0;
 				executionDepth--;
 			}
-			return;
 		}
-		plugin.getServer().getScheduler().runTask(plugin, command);
+	}
+
+	private void unsafeExecute(Runnable command) {
+		if (inEventLoop()) {
+			scheduledTasks.add(command);
+			tryRunLoop();
+		} else {
+			plugin.getServer().getScheduler().runTask(plugin, command);
+		}
+	}
+
+	@Override
+	public void execute(Runnable command) {
+		unsafeExecute(() -> {
+			try {
+				command.run();
+			} catch (Throwable e) {
+				plugin.getLogger().log(Level.SEVERE, "Error with task: ", e);
+			}
+		});
 	}
 
 	@Override
@@ -61,7 +94,7 @@ public class BukkitEventExecutor extends AbstractScheduledEventExecutor {
 
 	@Override
 	public boolean inEventLoop(Thread thread) {
-		if(thread.equals(Thread.currentThread())) {
+		if (thread.equals(Thread.currentThread())) {
 			return inEventLoop();
 		}
 		throw new IllegalStateException("Cannot check condition");
@@ -81,30 +114,14 @@ public class BukkitEventExecutor extends AbstractScheduledEventExecutor {
 	@Override
 	public <T> T invokeAny(Collection<? extends Callable<T>> tasks) throws InterruptedException, ExecutionException {
 		RunnableFuture<T> t = newTaskFor(tasks.iterator().next());
-		if(inEventLoop()) {
-			try {
-				t.run();
-			} catch(Throwable e) {
-				throw new ExecutionException(e);
-			}
-		} else {
-			execute(t);
-		}
+		unsafeExecute(t);
 		return t.get();
 	}
 
 	@Override
 	public <T> T invokeAny(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
 		FutureTask<T> t = new FutureTask<>(tasks.iterator().next());
-		if(inEventLoop()) {
-		    try {
-				t.run();
-			} catch(Throwable e) {
-				throw new ExecutionException(e);
-			}
-		} else {
-			execute(t);
-		}
+		unsafeExecute(t);
 		return t.get(timeout, unit);
 	}
 
@@ -128,7 +145,7 @@ public class BukkitEventExecutor extends AbstractScheduledEventExecutor {
 	public void shutdown() {
 		shuttingDown = true;
 		pollScheduledTask();
-		this.task.cancel();
+		this.scheduledTask.cancel();
 		terminationFuture.setSuccess(null);
 	}
 
@@ -143,5 +160,4 @@ public class BukkitEventExecutor extends AbstractScheduledEventExecutor {
 		return terminationFuture;
 	}
 
-	
 }
