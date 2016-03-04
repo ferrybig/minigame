5075f56b67755e95912411c1ef72346ba6004367
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2016-03-04 16:21:13 +0100

    Add more methods to bootstrap

--- src/main/java/me/ferrybig/javacoding/minecraft/minigame/bootstrap/Bootstrap.java
+++ src/main/java/me/ferrybig/javacoding/minecraft/minigame/bootstrap/Bootstrap.java
@@ -1,26 +1,30 @@
 package me.ferrybig.javacoding.minecraft.minigame.bootstrap;
 
+import io.netty.util.concurrent.EventExecutor;
 import io.netty.util.concurrent.Future;
 import me.ferrybig.javacoding.minecraft.minigame.AreaConstructor;
 import me.ferrybig.javacoding.minecraft.minigame.verrifier.AreaVerifier;
 import me.ferrybig.javacoding.minecraft.minigame.GameCore;
 import me.ferrybig.javacoding.minecraft.minigame.listener.GameListener;
 import me.ferrybig.javacoding.minecraft.minigame.configuration.FullConfig;
+import org.bukkit.plugin.Plugin;
 
 public interface Bootstrap {
 
-	public Bootstrap withLeaveCommand(String name);
-	
-	public Bootstrap withJoinCommand(String name);
-	
+	public Bootstrap withCoreAccessor(CoreAccessor accessor);
+
+	public Bootstrap withExecutor(EventExecutor executor);
+
+	public Bootstrap withPlugin(Plugin plugin);
+
 	public Bootstrap withGlobalGameListener(GameListener listen);
-	
+
 	public Bootstrap withConfig(FullConfig config);
-	
+
 	public Bootstrap withAreaVerifier(AreaVerifier verifier);
-	
+
 	public Bootstrap withAreaConstructor(AreaConstructor constructor);
-	
+
 	public Future<GameCore> build();
-	
+
 }
