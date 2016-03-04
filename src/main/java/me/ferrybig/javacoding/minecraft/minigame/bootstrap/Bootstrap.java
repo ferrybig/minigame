510a9206675d5e9e5fea3ae577aa792ed1df9ca9
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2016-03-04 15:18:27 +0100

    Remove unneeded methods

--- src/main/java/me/ferrybig/javacoding/minecraft/minigame/bootstrap/Bootstrap.java
+++ src/main/java/me/ferrybig/javacoding/minecraft/minigame/bootstrap/Bootstrap.java
@@ -1,15 +1,14 @@
 package me.ferrybig.javacoding.minecraft.minigame.bootstrap;
 
+import io.netty.util.concurrent.Future;
+import me.ferrybig.javacoding.minecraft.minigame.AreaConstructor;
 import me.ferrybig.javacoding.minecraft.minigame.verrifier.AreaVerifier;
 import me.ferrybig.javacoding.minecraft.minigame.GameCore;
 import me.ferrybig.javacoding.minecraft.minigame.listener.GameListener;
-import me.ferrybig.javacoding.minecraft.minigame.configuration.AreaConfig;
 import me.ferrybig.javacoding.minecraft.minigame.configuration.FullConfig;
 
 public interface Bootstrap {
 
-	public Bootstrap withConfigurationProvider(AreaConfig conf);
-	
 	public Bootstrap withLeaveCommand(String name);
 	
 	public Bootstrap withJoinCommand(String name);
@@ -20,6 +19,8 @@ public interface Bootstrap {
 	
 	public Bootstrap withAreaVerifier(AreaVerifier verifier);
 	
-	public GameCore build();
+	public Bootstrap withAreaConstructor(AreaConstructor constructor);
+	
+	public Future<GameCore> build();
 	
 }
