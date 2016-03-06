3f508567aaffc541b3dcf1aaa4ef884ebcb47d30
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2016-03-06 23:06:44 +0100

    Added validate method to AreaVerifier

--- src/main/java/me/ferrybig/javacoding/minecraft/minigame/verrifier/AreaVerifier.java
+++ src/main/java/me/ferrybig/javacoding/minecraft/minigame/verrifier/AreaVerifier.java
@@ -1,9 +1,12 @@
 
 package me.ferrybig.javacoding.minecraft.minigame.verrifier;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 import me.ferrybig.javacoding.minecraft.minigame.AreaInformation;
+import me.ferrybig.javacoding.minecraft.minigame.DefaultResolvedAreaInformation;
+import me.ferrybig.javacoding.minecraft.minigame.ResolvedAreaInformation;
 
 /**
  * Verifies if a area is correctly build for this minigame
@@ -16,5 +19,18 @@ public interface AreaVerifier {
 	public boolean isCorrect(AreaInformation area);
 
 	public Set<String> getValidTeams(AreaInformation area);
+	
+	public default ResolvedAreaInformation validate(AreaInformation area) {
+		boolean valid = isCorrect(area);
+		Set<String> teams;
+		if (valid) {
+			teams = getValidTeams(area);
+		} else {
+			teams = Collections.emptySet();
+		}
+		return new DefaultResolvedAreaInformation(area.getName(), area.isEnabled(), 
+				area.getDescription(), area.getTaggedBlocks(), area.getTaggedLocations(), 
+				area.getBounds(), area.maxPlayers(), valid, teams);
+	}
 
 }
