f210cb72fe842614a5e85f45fc7c399bfbd77260
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2016-03-06 20:09:59 +0100

    Added GameListenable for listenable areacontext events

---
+++ src/main/java/me/ferrybig/javacoding/minecraft/minigame/listener/GameListenable.java
@@ -0,0 +1,13 @@
+package me.ferrybig.javacoding.minecraft.minigame.listener;
+
+/**
+ *
+ * @author Fernando
+ */
+public interface GameListenable {
+
+	public boolean addListener(GameListener listener);
+
+	public boolean removeListener(GameListener listener);
+
+}
