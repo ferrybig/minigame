9329ca2e4ebf9d4e148437602540c662f62e5dad
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2016-03-07 13:46:48 +0100

    Rewrite GameRunner classes

--- src/main/java/me/ferrybig/javacoding/minecraft/minigame/AreaContextConstructor.java
+++ src/main/java/me/ferrybig/javacoding/minecraft/minigame/AreaContextConstructor.java
@@ -1,9 +1,9 @@
-
 package me.ferrybig.javacoding.minecraft.minigame;
 
 import io.netty.util.concurrent.Future;
 
 public interface AreaContextConstructor {
 
-	public Future<AreaContext> construct(ResolvedAreaInformation area);
+	public Future<AreaContext> construct(ResolvedAreaInformation area,
+			Controller controller, Pipeline pipeline);
 }
