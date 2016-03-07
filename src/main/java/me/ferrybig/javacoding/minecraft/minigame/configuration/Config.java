769e42278b382df0bc48fc57a7aae47b97d0033e
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2016-03-07 13:46:35 +0100

    Add documentation to Config

--- src/main/java/me/ferrybig/javacoding/minecraft/minigame/configuration/Config.java
+++ src/main/java/me/ferrybig/javacoding/minecraft/minigame/configuration/Config.java
@@ -1,11 +1,28 @@
-
 package me.ferrybig.javacoding.minecraft.minigame.configuration;
 
 import io.netty.util.concurrent.Future;
 
+/**
+ * Default interface for any config related classes
+ *
+ * @author Fernando
+ */
 public interface Config extends AutoCloseable {
+
+	/**
+	 * Flush changes to this config file to the disk, if required
+	 *
+	 * @return
+	 */
 	public Future<?> flushChanges();
-	
+
+	/**
+	 * Closes this config. A closed configuration cannot be interacted with.<p>
+	 * Configuration providers are free to ignore calls to this method.
+	 * <p>
+	 * Configuration providers should allow calls to this method even if the
+	 * config is closed. These calls must be ignored by the underlying system
+	 */
 	@Override
 	public void close();
 }
