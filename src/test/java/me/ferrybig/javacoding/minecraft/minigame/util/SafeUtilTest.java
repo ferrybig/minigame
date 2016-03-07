88942ab75cd281c73e32f0a61ea4df0754b960b2
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2016-03-07 16:59:36 +0100

    Added stub for SafeUtilTest

---
+++ src/test/java/me/ferrybig/javacoding/minecraft/minigame/util/SafeUtilTest.java
@@ -0,0 +1,19 @@
+package me.ferrybig.javacoding.minecraft.minigame.util;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Fernando
+ */
+public class SafeUtilTest {
+	
+	public SafeUtilTest() {
+	}
+
+	@Test
+	public void testSomeMethod() {
+	}
+	
+}
