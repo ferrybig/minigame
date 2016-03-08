59c7b402aedf34388778738907ed95a08bdf6d0c
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2016-03-08 18:44:59 +0100

    Extract PhaseContext to Triggerable

--- src/main/java/me/ferrybig/javacoding/minecraft/minigame/PhaseContext.java
+++ src/main/java/me/ferrybig/javacoding/minecraft/minigame/PhaseContext.java
@@ -1,45 +1,17 @@
 package me.ferrybig.javacoding.minecraft.minigame;
 
 import java.util.concurrent.ExecutorService;
-import me.ferrybig.javacoding.minecraft.minigame.messages.PlayerJoinMessage;
-import me.ferrybig.javacoding.minecraft.minigame.messages.PlayerLeaveMessage;
-import me.ferrybig.javacoding.minecraft.minigame.messages.PlayerPreJoinMessage;
-import me.ferrybig.javacoding.minecraft.minigame.messages.PlayerPreLeaveMessage;
-import me.ferrybig.javacoding.minecraft.minigame.messages.PlayerSpectateMessage;
-import me.ferrybig.javacoding.minecraft.minigame.messages.PlayerTeamMessage;
-import org.bukkit.OfflinePlayer;
-import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 
-public interface PhaseContext {
-	
+public interface PhaseContext extends Triggerable {
+
 	public AreaContext getAreaContext();
 
-	public boolean triggerExceptionCaucht(Throwable exception);
-	
-	public void triggerPlayerPreJoin(PlayerPreJoinMessage player);
-
-	public void triggerPlayerPreLeave(PlayerPreLeaveMessage player);
-	
-	public void triggerPlayerJoin(PlayerJoinMessage player);
-
-	public void triggerPlayerLeave(PlayerLeaveMessage player);
-	
-	public void triggerPlayerChangeTeam(PhaseContext area, PlayerTeamMessage player);
-
-	public void triggerPlayerSpectate(PhaseContext area, PlayerSpectateMessage player);
-	
-	public void triggerNextPhase();
-	
-	public void triggerReset();
-	
-	public void triggerUserEvent(Object event);
-	
 	public default ExecutorService getExecutor() {
 		return this.getAreaContext().getExecutor();
 	}
-	
+
 	public void registerNativeListener(Listener listener);
-	
+
 	public void unregisterNativeListener(Listener listener);
 }
---
+++ src/main/java/me/ferrybig/javacoding/minecraft/minigame/Triggerable.java
@@ -0,0 +1,32 @@
+package me.ferrybig.javacoding.minecraft.minigame;
+
+import me.ferrybig.javacoding.minecraft.minigame.messages.PlayerJoinMessage;
+import me.ferrybig.javacoding.minecraft.minigame.messages.PlayerLeaveMessage;
+import me.ferrybig.javacoding.minecraft.minigame.messages.PlayerPreJoinMessage;
+import me.ferrybig.javacoding.minecraft.minigame.messages.PlayerPreLeaveMessage;
+import me.ferrybig.javacoding.minecraft.minigame.messages.PlayerSpectateMessage;
+import me.ferrybig.javacoding.minecraft.minigame.messages.PlayerTeamMessage;
+
+public interface Triggerable {
+
+	boolean triggerExceptionCaucht(Throwable exception);
+
+	void triggerNextPhase();
+
+	void triggerPlayerChangeTeam(PhaseContext area, PlayerTeamMessage player);
+
+	void triggerPlayerJoin(PlayerJoinMessage player);
+
+	void triggerPlayerLeave(PlayerLeaveMessage player);
+
+	void triggerPlayerPreJoin(PlayerPreJoinMessage player);
+
+	void triggerPlayerPreLeave(PlayerPreLeaveMessage player);
+
+	void triggerPlayerSpectate(PhaseContext area, PlayerSpectateMessage player);
+
+	void triggerReset();
+
+	void triggerUserEvent(Object event);
+
+}
