4b6cfe570f4e0b86b6e36ec8ad66645b82dbd68a
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2016-03-15 14:01:02 +0100

    Add tests for DefaultAreaCreator

---
+++ src/test/java/me/ferrybig/javacoding/minecraft/minigame/core/DefaultAreaContextTest.java
@@ -0,0 +1,35 @@
+package me.ferrybig.javacoding.minecraft.minigame.core;
+
+import java.util.function.Function;
+import me.ferrybig.javacoding.minecraft.minigame.Area;
+import me.ferrybig.javacoding.minecraft.minigame.AreaCreator;
+import me.ferrybig.javacoding.minecraft.minigame.verrifier.AreaVerifier;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import static org.mockito.Mockito.verify;
+import org.mockito.MockitoAnnotations;
+
+/**
+ *
+ * @author Fernando
+ */
+public class DefaultAreaContextTest {
+
+	@Mock public Function<? super AreaCreator, Area> constructor;
+	@Mock public AreaVerifier verifier;
+
+	@Before
+	public void init() {
+		MockitoAnnotations.initMocks(this);
+	}
+
+	@Test
+	public void creatorCallSuperFunctionOnBuild() {
+		DefaultAreaCreator crea = new DefaultAreaCreator(constructor, verifier);
+		crea.setName("test");
+		crea.createArea();
+		verify(constructor).apply(crea);
+	}
+
+}
