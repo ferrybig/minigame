084f44aab26c85bddcac9e8bdf2ae4df1275e14b
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2016-03-15 16:17:03 +0100

    Add broadcastMessage

--- src/main/java/me/ferrybig/javacoding/minecraft/minigame/context/AreaContext.java
+++ src/main/java/me/ferrybig/javacoding/minecraft/minigame/context/AreaContext.java
@@ -10,29 +10,37 @@ import me.ferrybig.javacoding.minecraft.minigame.GameCore;
 import me.ferrybig.javacoding.minecraft.minigame.InformationContext;
 import me.ferrybig.javacoding.minecraft.minigame.Pipeline;
 import me.ferrybig.javacoding.minecraft.minigame.information.ResolvedAreaInformation;
+import org.bukkit.entity.Player;
 
 public interface AreaContext extends AttributeMap, ResolvedAreaInformation {
+
 	public Area getArea();
-	
+
 	public Pipeline pipeline();
-	
+
 	public EventExecutor getExecutor();
-	
+
 	public long instanceId();
-	
+
 	public Controller getController();
-	
+
 	public GameCore getCore();
-	
+
 	public default InformationContext getInformationContext() {
 		return getCore().getInfo();
 	}
-	
+
 	public default Future<?> getClosureFuture() {
 		return pipeline().getClosureFuture();
 	}
 
 	public default Logger getLogger() {
 		return getCore().getInfo().getLogger();
+	}
+
+	public default void sendBroadcastMessage(String message) {
+		for (Player p : getController().getPlayers().keySet()) {
+			p.sendMessage(message);
+		}
 	}
 }
