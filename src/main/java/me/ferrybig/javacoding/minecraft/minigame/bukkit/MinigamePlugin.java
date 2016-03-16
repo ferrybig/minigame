2177012d714e9350a7b6ac30059f8c89569b72cd
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2016-03-16 14:51:32 +0100

    format + add default options

--- src/main/java/me/ferrybig/javacoding/minecraft/minigame/bukkit/MinigamePlugin.java
+++ src/main/java/me/ferrybig/javacoding/minecraft/minigame/bukkit/MinigamePlugin.java
@@ -1,8 +1,8 @@
-
 package me.ferrybig.javacoding.minecraft.minigame.bukkit;
 
 import io.netty.util.concurrent.EventExecutor;
 import io.netty.util.concurrent.Future;
+import java.io.File;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
@@ -10,6 +10,10 @@ import me.ferrybig.javacoding.minecraft.minigame.information.AreaInformation;
 import me.ferrybig.javacoding.minecraft.minigame.GameCore;
 import me.ferrybig.javacoding.minecraft.minigame.Pipeline;
 import me.ferrybig.javacoding.minecraft.minigame.Bootstrap;
+import me.ferrybig.javacoding.minecraft.minigame.configuration.FileConfig;
+import me.ferrybig.javacoding.minecraft.minigame.context.SingleInstanceAreaContextConstructor;
+import me.ferrybig.javacoding.minecraft.minigame.core.DefaultArea;
+import me.ferrybig.javacoding.minecraft.minigame.core.DefaultAreaContext;
 import me.ferrybig.javacoding.minecraft.minigame.core.DefaultBootstrap;
 import me.ferrybig.javacoding.minecraft.minigame.executors.BukkitEventExecutor;
 import me.ferrybig.javacoding.minecraft.minigame.util.ChainedFuture;
@@ -21,7 +25,7 @@ public abstract class MinigamePlugin extends JavaPlugin implements GameCoreAcces
 	private EventExecutor executor;
 	private GameCore gameCore;
 	private Future<GameCore> gameCoreLoader;
-	
+
 	@Override
 	public void onEnable() {
 		super.onEnable();
@@ -35,32 +39,33 @@ public abstract class MinigamePlugin extends JavaPlugin implements GameCoreAcces
 		executor.shutdownGracefully();
 		super.onDisable();
 	}
-	
+
 	protected void onInternalLoad() {
-		gameCoreLoader = ChainedFuture.of(executor, ()->{
+		gameCoreLoader = ChainedFuture.of(executor, () -> {
 			Bootstrap b = new DefaultBootstrap();
 			this.initBasicSettings(b);
 			return executor.newSucceededFuture(b);
 		}).map(Bootstrap::build);
-		gameCoreLoader.addListener((Future<GameCore> c)-> {
+		gameCoreLoader.addListener((Future<GameCore> c) -> {
 			gameCore = c.get();
 			onGameCoreLoaded(gameCore);
 		});
 	}
-	
+
 	protected void onInternalUnload() {
 		if (gameCoreLoader != null) {
 			gameCoreLoader = null;
 		}
-		if(gameCore != null) {
+		if (gameCore != null) {
 			gameCore.close();
 			gameCore = null;
 		}
-		
+
 	}
-	
+
 	protected void initBasicSettings(Bootstrap bootstrap) {
 		bootstrap.withPlugin(this);
+		bootstrap.withLogger(this.getLogger());
 		bootstrap.withExecutor(executor);
 		bootstrap.withAreaVerifier(new AreaVerifier() {
 			@Override
@@ -78,13 +83,18 @@ public abstract class MinigamePlugin extends JavaPlugin implements GameCoreAcces
 				return true;
 			}
 		});
+		bootstrap.withAreaContextConstructor(new SingleInstanceAreaContextConstructor(
+				executor, DefaultAreaContext.factory(executor, this::initPipeline)));
+		bootstrap.withAreaConstructor(DefaultArea::new);
+		bootstrap.withConfig(new FileConfig(executor,
+				new File(this.getDataFolder(), "data.yml"), getServer()));
 		initPlugin(bootstrap);
 	}
-	
+
 	protected abstract void initPlugin(Bootstrap bootstrap);
-	
+
 	protected abstract void initPipeline(Pipeline pipeline);
-	
+
 	@Override
 	public boolean isLoaded() {
 		return gameCore != null;
@@ -92,8 +102,9 @@ public abstract class MinigamePlugin extends JavaPlugin implements GameCoreAcces
 
 	@Override
 	public GameCore getCore() {
-		if(!isLoaded())
+		if (!isLoaded()) {
 			throw new IllegalStateException("Game core not loaded yet");
+		}
 		return gameCore;
 	}
 
