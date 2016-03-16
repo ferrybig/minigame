7215859fd356d44faee0f57d885755666da26eba
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2016-03-16 10:36:00 +0100

    Add WaitingPhase

---
+++ src/main/java/me/ferrybig/javacoding/minecraft/minigame/phase/WaitingPhase.java
@@ -0,0 +1,32 @@
+package me.ferrybig.javacoding.minecraft.minigame.phase;
+
+import io.netty.util.AttributeKey;
+import io.netty.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+import me.ferrybig.javacoding.minecraft.minigame.context.PhaseContext;
+
+public class WaitingPhase extends DefaultPhase {
+
+	public static final AttributeKey<ScheduledFuture<?>> CURRENT_WAITING_LOOP
+			= AttributeKey.valueOf(WaitingPhase.class, "timeout");
+	private final int delayMiliseconds;
+
+	public WaitingPhase(int delayMiliseconds) {
+		this.delayMiliseconds = delayMiliseconds;
+	}
+
+	@Override
+	public void onPhaseUnload(PhaseContext area) throws Exception {
+		super.onPhaseUnload(area);
+		area.getAreaContext().attr(CURRENT_WAITING_LOOP).remove();
+	}
+
+	@Override
+	public void onPhaseLoad(PhaseContext area) throws Exception {
+		super.onPhaseLoad(area);
+		area.getAreaContext().attr(CURRENT_WAITING_LOOP).set(
+				area.getExecutor().schedule(() -> area.triggerNextPhase(),
+						delayMiliseconds, TimeUnit.MILLISECONDS));
+	}
+
+}
