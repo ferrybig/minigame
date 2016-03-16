fdf154321fdf0d14b39a6a8c1f5b6867748b3f70
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2016-03-16 13:06:53 +0100

    Update status sign class

--- src/main/java/me/ferrybig/javacoding/minecraft/minigame/status/StatusSign.java
+++ src/main/java/me/ferrybig/javacoding/minecraft/minigame/status/StatusSign.java
@@ -1,5 +1,14 @@
 package me.ferrybig.javacoding.minecraft.minigame.status;
 
+import org.bukkit.block.Block;
+
 public interface StatusSign {
 
+	public Block getLocation();
+
+	public SignType getType();
+
+	public enum SignType {
+		RANDOM, FIXED_AREA
+	}
 }
