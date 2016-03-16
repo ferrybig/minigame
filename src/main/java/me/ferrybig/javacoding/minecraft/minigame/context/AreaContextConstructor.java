668acb50d5eb55e16b2ab013773b49b2a1c33b8e
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2016-03-16 14:22:23 +0100

    Use Area argument instead of ResolvedAreaInformation

--- src/main/java/me/ferrybig/javacoding/minecraft/minigame/context/AreaContextConstructor.java
+++ src/main/java/me/ferrybig/javacoding/minecraft/minigame/context/AreaContextConstructor.java
@@ -1,13 +1,13 @@
 package me.ferrybig.javacoding.minecraft.minigame.context;
 
 import io.netty.util.concurrent.Future;
+import me.ferrybig.javacoding.minecraft.minigame.Area;
 import me.ferrybig.javacoding.minecraft.minigame.Controller;
 import me.ferrybig.javacoding.minecraft.minigame.GameCore;
 import me.ferrybig.javacoding.minecraft.minigame.Pipeline;
-import me.ferrybig.javacoding.minecraft.minigame.information.ResolvedAreaInformation;
 
 public interface AreaContextConstructor {
 
-	public Future<AreaContext> construct(GameCore core, ResolvedAreaInformation area,
+	public Future<AreaContext> construct(GameCore core, Area area,
 			Controller controller, Pipeline pipeline);
 }
