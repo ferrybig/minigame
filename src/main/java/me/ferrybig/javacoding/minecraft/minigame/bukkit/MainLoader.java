ef43238e8be6fb20ab0ef8ca5c405d63ddedbd0f
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2016-03-16 15:23:31 +0100

    Extract minigameplugin into seperate class for testing purposes

---
+++ src/main/java/me/ferrybig/javacoding/minecraft/minigame/bukkit/MainLoader.java
@@ -0,0 +1,113 @@
+
+package me.ferrybig.javacoding.minecraft.minigame.bukkit;
+
+import io.netty.util.concurrent.EventExecutor;
+import io.netty.util.concurrent.Future;
+import java.io.File;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import me.ferrybig.javacoding.minecraft.minigame.Bootstrap;
+import me.ferrybig.javacoding.minecraft.minigame.GameCore;
+import me.ferrybig.javacoding.minecraft.minigame.Pipeline;
+import me.ferrybig.javacoding.minecraft.minigame.configuration.FileConfig;
+import me.ferrybig.javacoding.minecraft.minigame.context.SingleInstanceAreaContextConstructor;
+import me.ferrybig.javacoding.minecraft.minigame.core.DefaultArea;
+import me.ferrybig.javacoding.minecraft.minigame.core.DefaultAreaContext;
+import me.ferrybig.javacoding.minecraft.minigame.core.DefaultBootstrap;
+import me.ferrybig.javacoding.minecraft.minigame.executors.BukkitEventExecutor;
+import me.ferrybig.javacoding.minecraft.minigame.information.AreaInformation;
+import me.ferrybig.javacoding.minecraft.minigame.util.ChainedFuture;
+import me.ferrybig.javacoding.minecraft.minigame.verrifier.AreaVerifier;
+import org.bukkit.plugin.Plugin;
+
+public abstract class MainLoader implements GameCoreAccessor {
+	private final Plugin plugin;
+	private EventExecutor executor;
+	private GameCore gameCore;
+	private Future<GameCore> gameCoreLoader;
+
+	public MainLoader(Plugin plugin) {
+		this.plugin = plugin;
+	}
+
+	public void onEnable() {
+		executor = new BukkitEventExecutor(plugin);
+		onInternalLoad();
+	}
+
+	public void onDisable() {
+		onInternalUnload();
+		executor.shutdownGracefully();
+	}
+
+	protected void onInternalLoad() {
+		gameCoreLoader = ChainedFuture.of(executor, () -> {
+			Bootstrap b = new DefaultBootstrap();
+			this.initBasicSettings(b);
+			return executor.newSucceededFuture(b);
+		}).map(Bootstrap::build);
+		gameCoreLoader.addListener((Future<GameCore> c) -> {
+			gameCore = c.get();
+			onGameCoreLoaded(gameCore);
+		});
+	}
+
+	protected void onInternalUnload() {
+		if (gameCoreLoader != null) {
+			gameCoreLoader = null;
+		}
+		if (gameCore != null) {
+			gameCore.close();
+			gameCore = null;
+		}
+	}
+
+	protected void initBasicSettings(Bootstrap bootstrap) {
+		bootstrap.withPlugin(plugin);
+		bootstrap.withLogger(plugin.getLogger());
+		bootstrap.withExecutor(executor);
+		bootstrap.withAreaVerifier(new AreaVerifier() {
+			@Override
+			public List<String> getProblems(AreaInformation area) {
+				return Collections.emptyList();
+			}
+
+			@Override
+			public Set<String> getValidTeams(AreaInformation area) {
+				return Collections.emptySet();
+			}
+
+			@Override
+			public boolean isCorrect(AreaInformation area) {
+				return true;
+			}
+		});
+		bootstrap.withAreaContextConstructor(new SingleInstanceAreaContextConstructor(
+				executor, DefaultAreaContext.factory(executor, this::initPipeline)));
+		bootstrap.withAreaConstructor(DefaultArea::new);
+		bootstrap.withConfig(new FileConfig(executor,
+				new File(plugin.getDataFolder(), "data.yml"), plugin.getServer()));
+		initPlugin(bootstrap);
+	}
+
+	protected abstract void initPlugin(Bootstrap bootstrap);
+
+	protected abstract void initPipeline(Pipeline pipeline);
+
+	@Override
+	public boolean isLoaded() {
+		return gameCore != null;
+	}
+
+	@Override
+	public GameCore getCore() {
+		if (!isLoaded()) {
+			throw new IllegalStateException("Game core not loaded yet");
+		}
+		return gameCore;
+	}
+
+	protected void onGameCoreLoaded(GameCore core) {
+	}
+}
--- src/main/java/me/ferrybig/javacoding/minecraft/minigame/bukkit/MinigamePlugin.java
+++ src/main/java/me/ferrybig/javacoding/minecraft/minigame/bukkit/MinigamePlugin.java
@@ -1,113 +1,75 @@
 package me.ferrybig.javacoding.minecraft.minigame.bukkit;
 
-import io.netty.util.concurrent.EventExecutor;
-import io.netty.util.concurrent.Future;
-import java.io.File;
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
-import me.ferrybig.javacoding.minecraft.minigame.information.AreaInformation;
 import me.ferrybig.javacoding.minecraft.minigame.GameCore;
 import me.ferrybig.javacoding.minecraft.minigame.Pipeline;
 import me.ferrybig.javacoding.minecraft.minigame.Bootstrap;
-import me.ferrybig.javacoding.minecraft.minigame.configuration.FileConfig;
-import me.ferrybig.javacoding.minecraft.minigame.context.SingleInstanceAreaContextConstructor;
-import me.ferrybig.javacoding.minecraft.minigame.core.DefaultArea;
-import me.ferrybig.javacoding.minecraft.minigame.core.DefaultAreaContext;
-import me.ferrybig.javacoding.minecraft.minigame.core.DefaultBootstrap;
-import me.ferrybig.javacoding.minecraft.minigame.executors.BukkitEventExecutor;
-import me.ferrybig.javacoding.minecraft.minigame.util.ChainedFuture;
-import me.ferrybig.javacoding.minecraft.minigame.verrifier.AreaVerifier;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public abstract class MinigamePlugin extends JavaPlugin implements GameCoreAccessor {
 
-	private EventExecutor executor;
-	private GameCore gameCore;
-	private Future<GameCore> gameCoreLoader;
+	protected final MainLoader main = new MainLoader(this) {
+		@Override
+		protected void initPipeline(Pipeline pipeline) {
+			MinigamePlugin.this.initPipeline(pipeline);
+		}
+
+		@Override
+		protected void initPlugin(Bootstrap bootstrap) {
+			MinigamePlugin.this.initPlugin(bootstrap);
+		}
+
+		@Override
+		protected void onGameCoreLoaded(GameCore core) {
+			super.onGameCoreLoaded(core);
+			MinigamePlugin.this.onGameCoreLoaded(core);
+		}
+
+		@Override
+		protected void initBasicSettings(Bootstrap bootstrap) {
+			super.initBasicSettings(bootstrap);
+			MinigamePlugin.this.initBasicSettings(bootstrap);
+		}
+
+		@Override
+		protected void onInternalUnload() {
+			super.onInternalUnload();
+			MinigamePlugin.this.onInternalLoad();
+		}
+
+		@Override
+		protected void onInternalLoad() {
+			super.onInternalLoad();
+			MinigamePlugin.this.onInternalUnload();
+		}
+
+	};
 
 	@Override
 	public void onEnable() {
 		super.onEnable();
-		executor = new BukkitEventExecutor(this);
-		onInternalLoad();
+		this.main.onEnable();
 	}
 
 	@Override
 	public void onDisable() {
-		onInternalUnload();
-		executor.shutdownGracefully();
+		this.main.onDisable();
 		super.onDisable();
 	}
 
-	protected void onInternalLoad() {
-		gameCoreLoader = ChainedFuture.of(executor, () -> {
-			Bootstrap b = new DefaultBootstrap();
-			this.initBasicSettings(b);
-			return executor.newSucceededFuture(b);
-		}).map(Bootstrap::build);
-		gameCoreLoader.addListener((Future<GameCore> c) -> {
-			gameCore = c.get();
-			onGameCoreLoaded(gameCore);
-		});
-	}
-
-	protected void onInternalUnload() {
-		if (gameCoreLoader != null) {
-			gameCoreLoader = null;
-		}
-		if (gameCore != null) {
-			gameCore.close();
-			gameCore = null;
-		}
-
-	}
-
-	protected void initBasicSettings(Bootstrap bootstrap) {
-		bootstrap.withPlugin(this);
-		bootstrap.withLogger(this.getLogger());
-		bootstrap.withExecutor(executor);
-		bootstrap.withAreaVerifier(new AreaVerifier() {
-			@Override
-			public List<String> getProblems(AreaInformation area) {
-				return Collections.emptyList();
-			}
-
-			@Override
-			public Set<String> getValidTeams(AreaInformation area) {
-				return Collections.emptySet();
-			}
-
-			@Override
-			public boolean isCorrect(AreaInformation area) {
-				return true;
-			}
-		});
-		bootstrap.withAreaContextConstructor(new SingleInstanceAreaContextConstructor(
-				executor, DefaultAreaContext.factory(executor, this::initPipeline)));
-		bootstrap.withAreaConstructor(DefaultArea::new);
-		bootstrap.withConfig(new FileConfig(executor,
-				new File(this.getDataFolder(), "data.yml"), getServer()));
-		initPlugin(bootstrap);
-	}
+	protected abstract void initPipeline(Pipeline pipeline);
 
 	protected abstract void initPlugin(Bootstrap bootstrap);
 
-	protected abstract void initPipeline(Pipeline pipeline);
-
-	@Override
-	public boolean isLoaded() {
-		return gameCore != null;
-	}
-
-	@Override
-	public GameCore getCore() {
-		if (!isLoaded()) {
-			throw new IllegalStateException("Game core not loaded yet");
-		}
-		return gameCore;
-	}
-
 	protected void onGameCoreLoaded(GameCore core) {
 	}
+
+	protected void initBasicSettings(Bootstrap bootstrap) {
+	}
+
+	protected void onInternalUnload() {
+	}
+
+	protected void onInternalLoad() {
+	}
+
 }
