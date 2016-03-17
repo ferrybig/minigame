d1a02fab07cabaed72b9206d9666b60e20388a2a
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2016-03-17 15:51:46 +0100

    Added AreaException

---
+++ src/main/java/me/ferrybig/javacoding/minecraft/minigame/exceptions/AreaException.java
@@ -0,0 +1,48 @@
+
+package me.ferrybig.javacoding.minecraft.minigame.exceptions;
+
+/**
+ *
+ * @author Fernando
+ */
+public class AreaException extends MinigameException {
+
+    /**
+     * Creates a new instance of <code>AreaException</code> without detail message.
+     */
+    public AreaException() {
+        super();
+    }
+
+
+    /**
+     * Constructs an instance of <code>AreaException</code> with the specified detail message.
+     * @param msg the detail message.
+     */
+    public AreaException(String msg) {
+        super(msg);
+    }
+
+    /**
+	 * Constructs an instance of <code>AreaException</code> with the specified
+	 * detail message and cause.
+	 *
+	 * @param message the detail message.
+	 * @param cause the cause
+	 */
+	public AreaException(String message, Throwable cause) {
+		super(message);
+        this.initCause(cause);
+	}
+
+	/**
+	 * Constructs an instance of <code>AreaException</code> with the specified
+	 * cause.
+	 *
+	 * @param cause the cause
+	 */
+	public AreaException(Throwable cause) {
+		this(cause.toString());
+        this.initCause(cause);
+	}
+}
