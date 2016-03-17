6967861379cab399fd55792ad1c4c31b83f61a3d
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2016-03-17 16:50:15 +0100

    Added getPlayer to PlayerMessage

--- src/main/java/me/ferrybig/javacoding/minecraft/minigame/messages/PlayerMessage.java
+++ src/main/java/me/ferrybig/javacoding/minecraft/minigame/messages/PlayerMessage.java
@@ -1,12 +1,17 @@
-
 package me.ferrybig.javacoding.minecraft.minigame.messages;
 
 import org.bukkit.entity.Player;
 
 public class PlayerMessage extends Message {
+
 	protected final Player player;
 
 	public PlayerMessage(Player player) {
 		this.player = player;
 	}
+
+	public Player getPlayer() {
+		return player;
+	}
+
 }
