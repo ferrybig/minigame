1877f81bb1e30c24c71a073ac78d39f198d7c6b2
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2016-03-22 16:57:49 +0100

    Expand tests

--- src/test/java/me/ferrybig/javacoding/minecraft/minigame/translation/BaseTranslationsAreFoundTest.java
+++ src/test/java/me/ferrybig/javacoding/minecraft/minigame/translation/BaseTranslationsAreFoundTest.java
@@ -2,8 +2,12 @@ package me.ferrybig.javacoding.minecraft.minigame.translation;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.stream.Collectors;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -11,7 +15,13 @@ import org.junit.runners.Parameterized;
 @RunWith(Parameterized.class)
 public class BaseTranslationsAreFoundTest {
 
-	private final static TranslationMap map = TranslationMap.getDefaultMappings();
+	private final static TranslationMap MAP = TranslationMap.getDefaultMappings();
+	private final static Set<String> SEEN_KEYS = new HashSet<>();
+
+	@BeforeClass
+	public static void beforeClass() {
+		SEEN_KEYS.clear();
+	}
 
 	@Parameterized.Parameters
 	public static Collection<Object[]> data() {
@@ -23,7 +33,12 @@ public class BaseTranslationsAreFoundTest {
 
 	@Test
 	public void keyExistsTest() {
-		String returnResult = map.getMessage(key, new Object[0]);
+		String returnResult = MAP.getMessage(key, new Object[0]);
 		assertNotNull("Could not find translation key " + key, returnResult);
+	}
+
+	@Test
+	public void keyAlreadyDefinedTest() {
+		assertTrue("Key already seen: " + key.key(), SEEN_KEYS.add(key.key()));
 	}
 }
