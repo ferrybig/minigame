fa1c99acb7c2b92e6f8da7d8632d5e825521e733
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2016-04-09 22:02:55 +0200

    Fixed compilation error

--- src/main/java/me/ferrybig/javacoding/minecraft/minigame/phase/impl/MessagePhase.java
+++ src/main/java/me/ferrybig/javacoding/minecraft/minigame/phase/impl/MessagePhase.java
@@ -52,7 +52,7 @@ public abstract class MessagePhase extends SkippedPhase {
 
 		@Override
 		protected void trigger(PhaseContext area, Collection<Player> players) {
-			String rawString = area.translate(message, args).split("\n");
+			String[] rawString = area.translate(message, args).split("\n");
 			for (Player p : players) {
 				p.sendMessage(rawString);
 			}
