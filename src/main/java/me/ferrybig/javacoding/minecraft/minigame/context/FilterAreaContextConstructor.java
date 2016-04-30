23e042c1b16d66fc20d09df75a674ee535283735
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2016-04-30 21:45:10 +0200

    Make FilterAreaContextConstructor pass events to the parent creator

--- src/main/java/me/ferrybig/javacoding/minecraft/minigame/context/FilterAreaContextConstructor.java
+++ src/main/java/me/ferrybig/javacoding/minecraft/minigame/context/FilterAreaContextConstructor.java
@@ -1,11 +1,22 @@
 package me.ferrybig.javacoding.minecraft.minigame.context;
 
+import io.netty.util.concurrent.Future;
+import me.ferrybig.javacoding.minecraft.minigame.Area;
+import me.ferrybig.javacoding.minecraft.minigame.Controller;
+import me.ferrybig.javacoding.minecraft.minigame.GameCore;
+import me.ferrybig.javacoding.minecraft.minigame.Pipeline;
+
 public abstract class FilterAreaContextConstructor implements AreaContextConstructor {
 
 	protected final AreaContextConstructor parent;
 
 	public FilterAreaContextConstructor(AreaContextConstructor parent) {
 		this.parent = parent;
+	}
+
+	@Override
+	public Future<AreaContext> construct(GameCore core, Area area, Controller controller, Pipeline pipeline) {
+		return this.parent.construct(core, area, controller, pipeline);
 	}
 
 }
