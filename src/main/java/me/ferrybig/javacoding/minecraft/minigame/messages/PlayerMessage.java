78fa4e8d0325fefc2f64a4fba21c4005bac97724
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2016-05-12 21:57:45 +0200

    Make PlayerMessage abstract

--- src/main/java/me/ferrybig/javacoding/minecraft/minigame/messages/PlayerMessage.java
+++ src/main/java/me/ferrybig/javacoding/minecraft/minigame/messages/PlayerMessage.java
@@ -2,7 +2,7 @@ package me.ferrybig.javacoding.minecraft.minigame.messages;
 
 import org.bukkit.entity.Player;
 
-public class PlayerMessage extends Message {
+public abstract class PlayerMessage extends Message {
 
 	protected final Player player;
 
