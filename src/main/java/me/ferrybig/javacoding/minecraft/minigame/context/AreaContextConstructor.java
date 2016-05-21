1148b602293d8684b4f6cf6b0f553d3bf2ea9339
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2016-05-21 22:57:25 +0200

    Add documentation

--- src/main/java/me/ferrybig/javacoding/minecraft/minigame/context/AreaContextConstructor.java
+++ src/main/java/me/ferrybig/javacoding/minecraft/minigame/context/AreaContextConstructor.java
@@ -6,6 +6,11 @@ import me.ferrybig.javacoding.minecraft.minigame.Controller;
 import me.ferrybig.javacoding.minecraft.minigame.GameCore;
 import me.ferrybig.javacoding.minecraft.minigame.Pipeline;
 
+/**
+ * Constructs a basic area instance from an area definition.
+ * This class should throw an exception when the area is invalid.
+ * @author Fernando
+ */
 public interface AreaContextConstructor {
 
 	public Future<AreaContext> construct(GameCore core, Area area,
