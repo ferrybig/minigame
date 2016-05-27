b5974f0eb121441293deda921528d28c578faec8
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2016-05-27 22:16:15 +0200

    added fromCallable to ExceptionRunnable

--- src/main/java/me/ferrybig/javacoding/minecraft/minigame/util/ExceptionRunnable.java
+++ src/main/java/me/ferrybig/javacoding/minecraft/minigame/util/ExceptionRunnable.java
@@ -1,7 +1,14 @@
 package me.ferrybig.javacoding.minecraft.minigame.util;
 
+import java.util.concurrent.Callable;
+
 public interface ExceptionRunnable {
 
 	public void run() throws Exception;
 
+	public static ExceptionRunnable fromCallable(Callable<?> call) {
+		return ()->{
+			call.call();
+		};
+	}
 }
