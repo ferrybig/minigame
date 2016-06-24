3de5c9438ee3b14c278287336af39a990fb6daeb
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2016-06-24 23:47:19 +0200

    Added javadoc

--- src/main/java/me/ferrybig/javacoding/minecraft/minigame/context/FilterAreaContextConstructor.java
+++ src/main/java/me/ferrybig/javacoding/minecraft/minigame/context/FilterAreaContextConstructor.java
@@ -7,14 +7,25 @@ import me.ferrybig.javacoding.minecraft.minigame.Controller;
 import me.ferrybig.javacoding.minecraft.minigame.GameCore;
 import me.ferrybig.javacoding.minecraft.minigame.Pipeline;
 
+/**
+ * Simple class that delegates its event to a parent class
+ * @author Fernando
+ */
 public abstract class FilterAreaContextConstructor implements AreaContextConstructor {
 
 	protected final AreaContextConstructor parent;
 
+	/**
+	 * COnstructs a FilterAreaContextConstructor
+	 * @param parent AreaContextConstructor to delegate calls to, may not be null
+	 */
 	public FilterAreaContextConstructor(AreaContextConstructor parent) {
 		this.parent = Objects.requireNonNull(parent, "parent == null");
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public Future<AreaContext> construct(GameCore core, Area area, Controller controller, Pipeline pipeline) {
 		return this.parent.construct(core, area, controller, pipeline);
