f758d56e89250f2c52d3d1e2179430f3986649ed
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2016-06-30 22:48:44 +0200

    Block EnityHangingEvent

--- src/main/java/me/ferrybig/javacoding/minecraft/minigame/phase/impl/EditPreventingPhase.java
+++ src/main/java/me/ferrybig/javacoding/minecraft/minigame/phase/impl/EditPreventingPhase.java
@@ -4,10 +4,12 @@ import java.util.Optional;
 import me.ferrybig.javacoding.minecraft.minigame.Controller.PlayerInfo;
 import me.ferrybig.javacoding.minecraft.minigame.context.PhaseContext;
 import me.ferrybig.javacoding.minecraft.minigame.phase.DefaultPhase;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.hanging.HangingBreakByEntityEvent;
 import org.bukkit.event.player.PlayerBucketEmptyEvent;
 import org.bukkit.event.player.PlayerBucketFillEvent;
 
@@ -40,6 +42,17 @@ public class EditPreventingPhase extends DefaultPhase {
 				return;
 			}
 			evt.setCancelled(true);
+		}
+
+		@EventHandler
+		public void onEvent(HangingBreakByEntityEvent evt) {
+			if(evt.getRemover() instanceof Player) {
+				Optional<PlayerInfo> player = area.getController().getPlayer((Player)evt.getRemover());
+				if (!player.isPresent()) {
+					return;
+				}
+				evt.setCancelled(true);
+			}
 		}
 
 		@EventHandler
